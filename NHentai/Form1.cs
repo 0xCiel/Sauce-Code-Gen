0dbe8dd49676e53fd62797a45ea3a463dd60f30f
Ciel <89581874+0xCiel@users.noreply.github.com>
2025-04-24 12:15:24 +0900

    Update Form1.cs

--- NHentai/Form1.cs
+++ NHentai/Form1.cs
@@ -5,21 +5,35 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using NHentaiAPI;
 using NHentaiAPI.Models.Books;
+
 namespace NHentai
 {
     public partial class Form1 : Form
     {
         private readonly NHentaiClient _client;
-        private Book _currentBook; 
-        private int _currentGalleryIndex = 1; 
-
+        private Book _currentBook;
+        private int _currentGalleryIndex = 1;
+        private Size formOriginalSize;
+        private Rectangle recGenButton;
+        private Rectangle recThumbnailImage;
+        private Rectangle recImages;
+        private Rectangle recGeneratedCode;
+        private Rectangle recVScrollBar;
+        private Rectangle recPageLabel;
         public Form1()
         {
             InitializeComponent();
             _client = new NHentaiClient(""); //put your user agent here https://www.whatismybrowser.com/detect/what-is-my-user-agent/
-
+            this.Resize += Form1_Resize;
+            formOriginalSize = this.Size;
+            recGenButton = new Rectangle(GenButton.Location, GenButton.Size);
+            recThumbnailImage = new Rectangle(ThumbnailImage.Location, ThumbnailImage.Size);
+            recImages = new Rectangle(Images.Location, Images.Size);
+            recGeneratedCode = new Rectangle(GeneratedCode.Location, GeneratedCode.Size);
+            recVScrollBar = new Rectangle(vScrollBar1.Location, vScrollBar1.Size);
+            recPageLabel = new Rectangle(PageLabel.Location, PageLabel.Size);
         }
-        
+
         private async void GenButton_Click(object sender, EventArgs e)
         {
             try
@@ -46,6 +60,7 @@ namespace NHentai
                 MessageBox.Show($"Failed to fetch book details or gallery: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
         private async Task DisplayGalleryThumbnailAsync()
         {
             try
@@ -69,12 +84,12 @@ namespace NHentai
             {
                 if (_currentBook != null && e.NewValue > e.OldValue && _currentGalleryIndex < _currentBook.NumPages)
                 {
-                    _currentGalleryIndex++; 
+                    _currentGalleryIndex++;
                     await DisplayGalleryThumbnailAsync();
                 }
                 else if (_currentBook != null && e.NewValue < e.OldValue && _currentGalleryIndex > 1)
                 {
-                    _currentGalleryIndex--; 
+                    _currentGalleryIndex--;
                     await DisplayGalleryThumbnailAsync();
                 }
             }
@@ -94,6 +109,30 @@ namespace NHentai
             {
                 PageLabel.Text = "Page: 0/0";
             }
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            ResizeControl(GenButton, recGenButton);
+            ResizeControl(ThumbnailImage, recThumbnailImage);
+            ResizeControl(Images, recImages);
+            ResizeControl(GeneratedCode, recGeneratedCode);
+            ResizeControl(vScrollBar1, recVScrollBar);
+            ResizeControl(PageLabel, recPageLabel);
+        }
+
+        private void ResizeControl(Control control, Rectangle originalRect)
+        {
+            float xRatio = (float)this.Width / formOriginalSize.Width;
+            float yRatio = (float)this.Height / formOriginalSize.Height;
+
+            int newX = (int)(originalRect.X * xRatio);
+            int newY = (int)(originalRect.Y * yRatio);
+
+            int newWidth = (int)(originalRect.Width * xRatio);
+            int newHeight = (int)(originalRect.Height * yRatio);
+
+            control.Location = new Point(newX, newY);
+            control.Size = new Size(newWidth, newHeight);
         }
     }
 }
